1b1eb83f4684194a28ee2b47b4328fd70507d507
Henk van der Kuur <info@vanderkuur.com>
2020-10-29 11:50:54 +0100

    foutmelding met filenaam

--- src/FilesInlezen.java
+++ src/FilesInlezen.java
@@ -1,3 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen {
+    public static void main(String[] args) {
+
+        File f = new File("Hello2.txt");
+
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine())
+                System.out.println(s.nextLine());
+        } catch (FileNotFoundException a) {
+            System.out.println("Het bestand met de naam: "+f+" Is niet gevonden");
+        } finally {
+            System.out.println("Tot ziens!");
+        }
+    }
 }
+
